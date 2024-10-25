bc673818b797877aa98e02bcdc6d1affc01fb36b
konika098 <konikakhan2000@gmail.com>
2024-10-25 16:10:40 +0600

    fixed blog and testimonial issues

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,85 +1,140 @@
+// Blog data
 export const blogData = [
-  {
+    {
       img: "/public/assets/blog-img-1.png",
       category: "Marketing",
       readTime: "2 min read",
       title: "How To Create A Modern That Attracts Customers?",
-      link: "./blog.html"
-  },
-  {
+      link: "./blog.html",
+    },
+    {
       img: "/public/assets/blog-img-2.png",
       category: "Marketing",
       readTime: "2 min read",
       title: "How To Create A Modern That Attracts Customers?",
-      link: "./blog.html"
-  },
-  {
+      link: "./blog.html",
+    },
+    {
       img: "/public/assets/blog-img-1.png",
       category: "Marketing",
       readTime: "2 min read",
       title: "How To Create A Modern That Attracts Customers?",
-      link: "./blog.html"
-  },
-  {
+      link: "./blog.html",
+    },
+    {
       img: "/public/assets/blog-img-2.png",
       category: "Marketing",
       readTime: "2 min read",
       title: "How To Create A Modern That Attracts Customers?",
-      link: "./blog.html"
-  }
-];
-
-export const footerData = {
-  footerMenus: [
+      link: "./blog.html",
+    },
+  ];
+  
+  // Footer data
+  export const footerData = {
+    footerMenus: [
       {
-          title: "Company",
-          links: [
-              { label: "Home", href: "/" },
-              { label: "About us", href: "/about" },
-              { label: "Services", href: "/service" },
-              { label: "Blog", href: "/blog" },
-              { label: "Contact", href: "/contact" }
-          ]
+        title: "Company",
+        links: [
+          { label: "Home", href: "/" },
+          { label: "About us", href: "/about" },
+          { label: "Services", href: "/service" },
+          { label: "Blog", href: "/blog" },
+          { label: "Contact", href: "/contact" },
+        ],
       },
       {
-          title: "Support",
-          links: [
-              { label: "Getting started", href: "#" },
-              { label: "Help center", href: "#" },
-              { label: "Server status", href: "#" },
-              { label: "Report a bug", href: "#" },
-              { label: "Chat support", href: "#" }
-          ]
-      }
-  ],
-  contactDetails: [
+        title: "Support",
+        links: [
+          { label: "Getting started", href: "#" },
+          { label: "Help center", href: "#" },
+          { label: "Server status", href: "#" },
+          { label: "Report a bug", href: "#" },
+          { label: "Chat support", href: "#" },
+        ],
+      },
+    ],
+    contactDetails: [
       {
-          icon: "fa-regular fa-envelope",
-          label: "contact@company.com",
-          href: "mailto:naharweb15@gmail.com"
+        icon: "fa-regular fa-envelope",
+        label: "contact@company.com",
+        href: "mailto:naharweb15@gmail.com",
       },
       {
-          icon: "fa-solid fa-phone",
-          label: "(414) 687 - 5892",
-          href: "tel:+8801978159172"
+        icon: "fa-solid fa-phone",
+        label: "(414) 687 - 5892",
+        href: "tel:+8801978159172",
       },
       {
-          icon: "fa-solid fa-location-dot",
-          label: "794 Mcallister St, San Francisco, 94102",
-          href: "https://www.google.com/maps"
-      }
-  ],
-  socialLinks: [
+        icon: "fa-solid fa-location-dot",
+        label: "794 Mcallister St, San Francisco, 94102",
+        href: "https://www.google.com/maps",
+      },
+    ],
+    socialLinks: [
       {
-          icon: "fa-brands fa-facebook-f",
-          href: "https://www.facebook.com/Opediatech/"
+        icon: "fa-brands fa-facebook-f",
+        href: "https://www.facebook.com/Opediatech/",
       },
       { icon: "fa-brands fa-twitter", href: "https://twitter.com/" },
       {
-          icon: "fa-brands fa-instagram",
-          href: "https://www.instagram.com/opedia_technologies/"
+        icon: "fa-brands fa-instagram",
+        href: "https://www.instagram.com/opedia_technologies/",
       },
       { icon: "fa-brands fa-linkedin-in", href: "https://bd.linkedin.com/" },
-      { icon: "fa-brands fa-youtube", href: "https://www.youtube.com/" }
-  ]
-};
+      { icon: "fa-brands fa-youtube", href: "https://www.youtube.com/" },
+    ],
+  };
+  
+
+  export const testimonialsData = [
+    {
+      quote: "High standard and excellent quality of work. They helped my business grow in digital.",
+      name: "Adam Watson",
+      role: "Digital Marketer",
+      ratingImage: "/public/assets/rating.png",
+      quoteImage: "/public/assets/quote.png",
+      aos: "fade-right",
+    },
+    {
+      quote: "Awesome work! High standard and excellent quality. They helped my business grow in digital.",
+      name: "Eloise Smith",
+      role: "Content Marketer",
+      ratingImage: "/public/assets/rating.png",
+      quoteImage: "/public/assets/quote.png",
+      aos: "fade-up",
+    },
+    {
+      quote: "Awesome work! High standard and excellent quality. They helped my business grow in digital.",
+      name: "Eloise Smith",
+      role: "Content Marketer",
+      ratingImage: "/public/assets/rating.png",
+      quoteImage: "/public/assets/quote.png",
+      aos: "fade-up",
+    },
+    {
+      quote: "High standard and excellent quality of work. They helped my business grow in digital.",
+      name: "Adam Watson",
+      role: "Digital Marketer",
+      ratingImage: "/public/assets/rating.png",
+      quoteImage: "/public/assets/quote.png",
+      aos: "fade-right",
+    },
+    {
+      quote: "Awesome work! High standard and excellent quality. They helped my business grow in digital.",
+      name: "Eloise Smith",
+      role: "Content Marketer",
+      ratingImage: "/public/assets/rating.png",
+      quoteImage: "/public/assets/quote.png",
+      aos: "fade-up",
+    },
+    {
+      quote: "Awesome work! High standard and excellent quality. They helped my business grow in digital.",
+      name: "Eloise Smith",
+      role: "Content Marketer",
+      ratingImage: "/public/assets/rating.png",
+      quoteImage: "/public/assets/quote.png",
+      aos: "fade-up",
+    },
+  ];
+  
