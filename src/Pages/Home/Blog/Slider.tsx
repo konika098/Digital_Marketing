18f74934c3daac85f485e7a3762b1800fdf28d50
konika098 <konikakhan2000@gmail.com>
2024-10-25 14:52:46 +0600

    add link tag

--- src/Pages/Home/Blog/Slider.tsx
+++ src/Pages/Home/Blog/Slider.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import '../../Home/Blog/Slider.css';
-import { Pagination } from 'swiper/modules';
-import { blogData } from '../../../utils/data'; 
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../Home/Blog/Slider.css";
+import { Pagination } from "swiper/modules";
+import { blogData } from "../../../utils/data";
 import angelIcon from "/public/assets/angel-icon.png";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Swiper 
-        slidesPerView={2} 
-        pagination={{ clickable: true }} 
-        modules={[Pagination]} 
+      <Swiper
+        slidesPerView={2}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
         className="mySwiper"
       >
         {blogData.map((blog, index) => (
-          <SwiperSlide key={index}> 
+          <SwiperSlide key={index}>
             <div className="blog-wrapper">
               <div className="blog-img-parent">
                 <img className="img-fluid" src={blog.img} alt="Blog" />
@@ -27,14 +28,21 @@ export default function App() {
                 <span className="fs-18-c blog-sub">{blog.readTime}</span>
               </div>
               <div className="blog-sub-2-parent">
-                <p className="fs-32-c text-start">{blog.title.split(' ').map((word, i) => (
-                  <span key={i}>
-                    {word} {i % 5 === 4 && <br className="d-none d-lg-block" />} 
-                  </span>
-                ))}</p>
-                <a href={blog.link}>
-                  <img className="img-fluid animatedImg" src={angelIcon} alt="icon" />
-                </a>
+                <p className="fs-32-c text-start">
+                  {blog.title.split(" ").map((word, i) => (
+                    <span key={i}>
+                      {word}{" "}
+                      {i % 5 === 4 && <br className="d-none d-lg-block" />}
+                    </span>
+                  ))}
+                </p>
+                <Link to={blog.link}>
+                  <img
+                    className="img-fluid animatedImg"
+                    src={angelIcon}
+                    alt="icon"
+                  />
+                </Link>
               </div>
             </div>
           </SwiperSlide>
