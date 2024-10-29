d40ef1b1a3168a0da527e95fad1c30ddb2a57ab7
konika098 <konikakhan2000@gmail.com>
2024-10-29 13:28:12 +0600

    project setup update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Main from './Layout/Main';
+import About from './Pages/About/About';
+import Blog from './Pages/Blog/Blog';
+import Service from './Pages/Service/Service';
+import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
+import Contact from './Pages/Contact/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/blog",
+        element:<Blog/>
+      },
+      {
+        path:"/service",
+        element:<Service/>
+      },
+      {
+        path:"/service-details",
+        element:<ServiceDetails/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      }
+    ]
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
+</>
+
+      
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,11 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
-import * as React from "react";
+import App from './App';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './Pages/Home/Home';
-import Main from './Layout/Main';
-import About from './Pages/About/About';
-import Blog from './Pages/Blog/Blog';
-import Service from './Pages/Service/Service';
-import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
-import Contact from './Pages/Contact/Contact';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main/>,
-    children:[
-      {
-        path:"/",
-        element:<Home/>
-      },
-      {
-        path:"/about",
-        element:<About/>
-      },
-      {
-        path:"/blog",
-        element:<Blog/>
-      },
-      {
-        path:"/service",
-        element:<Service/>
-      },
-      {
-        path:"/service-details",
-        element:<ServiceDetails/>
-      },
-      {
-        path:"/contact",
-        element:<Contact/>
-      }
-    ]
-  },
-]);
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
