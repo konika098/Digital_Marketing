bde4c818f9c870d1533004e7b56e735bad28d9ba
konika098 <konikakhan2000@gmail.com>
2024-10-30 16:10:14 +0600

    add scroll to top function

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "../Components/SharedComponent/Footer";
 import Menubar from "../Components/SharedComponent/Menubar";
+import ScrollToTop from "../Components/ScrollToTop";
 
 const Main = () => {
   return (
     <>
+      <ScrollToTop />
       <Menubar />
       <Outlet />
       <Footer />
